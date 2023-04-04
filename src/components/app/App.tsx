4b69cf129013015f58aa6567d7201b1cf8ddf4eb
VanyaLyashuk <ivan129522@gmail.com>
2023-04-04 19:19:24 +0300

    add our favorite section

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -6,10 +6,21 @@ import './App.scss'
 function App() {
   return (
     <div className="App text-3xl">
-      <Banner gist="sale"/>
-      <Navbar />
+      <header className="header">
+        <Banner gist="sale"/>
+        <Navbar />
+      </header>
       <Promo />
       <Banner gist="note"/>
+      <main className="main">
+        <section className="our-favorites section">
+          <div className="container mx-auto">
+            <h2 className="title our-favorites__title">
+              Our Favorites
+            </h2>
+          </div>
+        </section>
+      </main>
     </div>
   )
 }
